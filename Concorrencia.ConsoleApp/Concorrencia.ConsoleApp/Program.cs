69c448346db35900d916cdf64ef7717e1993ce0f
willudwig <acesso.william@gmail.com>
2023-10-16 19:15:08 -0300

    criado o projeto console

---
+++ Concorrencia.ConsoleApp/Concorrencia.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Concorrencia.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
