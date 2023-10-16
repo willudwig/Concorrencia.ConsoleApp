db0f9b97015bc41b4a1e786370e1f09347beb483
willudwig <acesso.william@gmail.com>
2023-10-16 19:21:28 -0300

    projeto trazido para este

---
+++ Concorrencia.ConsoleApp/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Concorrencia.ConsoleApp
+{
+    internal class Animal
+    {
+        public string Nome { get; set; }
+
+        public Animal()
+        {
+            Nome = ObterAnimal();
+        }
+
+        string ObterAnimal()
+        {
+            List<string> lista = new List<string>();
+            lista.Add("onca");
+            lista.Add("tigre");
+            lista.Add("leopardo");
+            lista.Add("leao");
+            lista.Add("tigre");
+            lista.Add("pantera");
+            lista.Add("lobo-guara");
+
+            Random random = new Random();
+
+            // Gerar um número aleatório inteiro no intervalo de 0 a 7
+            int numeroAleatorio = random.Next(0, 7);                       
+
+            return lista[numeroAleatorio];
+        }
+    }
+}
---
+++ Concorrencia.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concorrencia.ConsoleApp
+{
+    internal class Program
+    {
+        private static object lockObject = new object();        
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+                Task.Run(() => { GetNewAnimal(); });
+
+            //Task.WaitAll();
+            //Task.WhenAll();           
+            
+            Thread.Sleep(19000);
+            Console.WriteLine("\nTodos os animais já comeram!");   
+        }
+
+        static void GetNewAnimal()
+        {
+            var animal = new Animal().Nome;
+
+            // a seção crítica (que pode ter muitos acessos ao mesmo tempo)deve ficar dentro do lock
+            // O lock é usado para garantir que apenas uma thread por vez possa acessar a seção crítica do código, evitando a concorrência
+            lock (lockObject)
+            {
+                Console.WriteLine($"{(animal)} vai agora comer a carne | ID: {Task.CurrentId}"); //Thread.CurrentThread.ManagedThreadId
+                Thread.Sleep(1500);
+            }
+        }
+
+        /* outra forma de escrever uma thread
+         * 
+        Thread thread1 = new Thread(GetNewAnimal);
+        Thread thread2 = new Thread(GetNewAnimal);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        hreads são unidades de execução em um processo, a concorrência refere-se à coordenação de várias tarefas intercaladas para melhorar a eficiência,
+        enquanto o paralelismo envolve a execução real de múltiplas tarefas ao mesmo tempo, frequentemente em núcleos de CPU separados. Esses conceitos
+        são frequentemente usados em conjunto para criar sistemas eficientes e responsivos.
+         */
+
+    }
+}
